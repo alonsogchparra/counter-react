efe4640cb959e6cf11b870c74e8200467a0517ae
Alonso Parra <alonsogparra@gmail.com>
2019-04-06 19:23:16 -0400

    Zoom effect added

--- counter_material-ui/src/components/CounterOutput/CounterOutput.js
+++ counter_material-ui/src/components/CounterOutput/CounterOutput.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
+import Zoom from '@material-ui/core/Zoom';
 
 const counterOutput = (props) => (
   <AppBar position="static">
-    <h1>Current Value: {props.value}</h1>
+    <Zoom in={props.activated} timeout={1000}>
+      <h1>Current Value: {props.value}</h1>
+    </Zoom>
   </AppBar>
 );
 
