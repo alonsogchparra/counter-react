a9c2f7db87492f97f589fe1fd38f3cb58afe8408
Alonso Parra <alonsogparra@gmail.com>
2019-04-06 18:20:35 -0400

    Updating Counter wiht element from material-ui like Grid, Paper, Button and some styles on the container

--- counter_material-ui/src/containers/Counter/Counter.js
+++ counter_material-ui/src/containers/Counter/Counter.js
@@ -1,37 +1,94 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 import CounterControl from '../../components/CounterControl/CounterControl';
 import * as actionCreators from '../../store/actions';
 
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    marginTop: theme.spacing.unit * 3,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    width: '50%',
+  },
+  button: {
+    marginTop: theme.spacing.unit * 4
+  }
+});
+
 class Counter extends Component {
   render () {
+
+    const {classes} = this.props;
+
     return (
       <div>
         <CounterOutput value={this.props.counter} />
 
-        <CounterControl
-          label="Increment"
-          clicked={this.props.onIncrementCounter}
-        />
+        <div className={classes.root}>
 
-        <CounterControl
-          label="Decrement"
-          clicked={this.props.onDecrementCounter}
-        />
+          <Grid container justify="center" direction="row">
 
-        <CounterControl
-          label="Add"
-          clicked={this.props.onAddCounter}
-        />
+            <Paper className={classes.paper}>
 
-        <CounterControl
-          label="Subtract"
-          clicked={this.props.onSubtractCounter}
-        />
+              <Grid container justify="center" direction="row">
 
-        <button onClick={() => this.props.onStoreResult(this.props.counter)}>Show Result</button>
+                <Grid item xs={12} md={3}>
+                  <CounterControl
+                    label="Increment"
+                    clicked={this.props.onIncrementCounter}
+                  />
+                </Grid>
+
+                <Grid item xs={12} md={3}>
+                  <CounterControl
+                    label="Decrement"
+                    clicked={this.props.onDecrementCounter}
+                  />
+                </Grid>
+
+                <Grid item xs={12} md={3}>
+                  <CounterControl
+                    label="Add"
+                    clicked={this.props.onAddCounter}
+                  />
+                </Grid>
+
+                <Grid item xs={12} md={3}>
+                  <CounterControl
+                    label="Subtract"
+                    clicked={this.props.onSubtractCounter}
+                  />
+                </Grid>
+
+              </Grid>
+
+            </Paper>
+
+          </Grid>
+
+          <div className={classes.button}>
+            <Button
+              onClick={() => this.props.onStoreResult(this.props.counter)}
+              size="large"
+              color="primary"
+              variant="contained"
+            >
+            Show Result
+            </Button>
+          </div>
+
+
+
+        </div>
 
         <ul>
           {this.props.results.map(result => (
@@ -62,4 +119,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(Counter));
