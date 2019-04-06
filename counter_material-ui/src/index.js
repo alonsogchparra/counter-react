dbc9bd9a13218464a40baa66163d17a0ab5e6754
Alonso Parra <alonsogparra@gmail.com>
2019-04-06 12:42:12 -0400

    index.js modified to work with redux and redux-thunk

--- counter_material-ui/src/index.js
+++ counter_material-ui/src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore, combineReducers, compose } from 'redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[Middleware Dispatching]', action);
+      const result = next(action);
+      console.log('[Middleware next state]', store.getState());
+      return result;
+    }
+  }
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)));
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
