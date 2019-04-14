7eb2ac29bd7e8c7ab1b22b426a4d606b38fe780e
Alonso Parra <alonsogparra@gmail.com>
2019-04-14 17:23:18 -0400

    rootReducer updated with result reducer

--- counter_semantic_ui/src/index.js
+++ counter_semantic_ui/src/index.js
@@ -7,10 +7,12 @@ import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import counterReducer from './store/reducers/counter';
+import resultReducer from './store/reducers/result';
 import * as serviceWorker from './serviceWorker';
 
 const rootReducer = combineReducers({
-  ctr: counterReducer
+  ctr: counterReducer,
+  res: resultReducer
 });
 
 const logger = store => {
