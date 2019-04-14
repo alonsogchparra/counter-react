5520bb95a2fd02b5d95f5c370819f682c423beea
Alonso Parra <alonsogparra@gmail.com>
2019-04-14 18:44:54 -0400

    Adding Semantic-UI Elements and also adding the new components plus the loading state from resultReducer

--- counter_semantic_ui/src/containers/Counter/Counter.js
+++ counter_semantic_ui/src/containers/Counter/Counter.js
@@ -1,26 +1,71 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Divider, Button, Grid, Card, Container, Segment, Icon } from 'semantic-ui-react';
 
-import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import CounterConsole from '../../components/CounterConsole/CounterConsole';
+import CounterShowResult from '../../components/CounterShowResult/CounterShowResult';
 import * as actionCreators from '../../store/actions';
+
 
 class Counter extends Component {
   render () {
+
+    let showResult = (
+      this.props.results.map(result => (
+        <Grid.Column key={result.id}>
+          <Card
+            fluid
+            onClick={() => this.props.onDeleteResult(result.id)}
+          >
+            <Card.Content>
+              <Card.Header textAlign='center'>{result.value}</Card.Header>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+      ))
+    );
+
+    if (this.props.loading) {
+      showResult = (
+        <Container>
+          <Segment inverted padded='very'>
+            <Grid textAlign='center'>
+              <Icon.Group size='large'>
+                <Icon loading size='huge' name='circle notch' />
+                <Icon name='calculator' />
+              </Icon.Group>
+            </Grid>
+          </Segment>
+        </Container>
+      );
+    }
+
     return (
       <div>
         <CounterOutput value={this.props.counter} />
-        <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-        <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
-        <CounterControl label="Add" clicked={this.props.onAddCounter} />
-        <CounterControl label="Subtract" clicked={this.props.onSubtractCounter} />
-        <button onClick={() => this.props.onStoreResult(this.props.counter)}>Show Result</button>
 
-        <ul>
-          {this.props.results.map(result => (
-            <li key={result.id} onClick={() => this.props.onDeleteResult(result.id)}>{result.value}</li>
-          ))}
-        </ul>
+        <Divider />
+
+        <CounterConsole
+          incremented={this.props.onIncrementCounter}
+          decremented={this.props.onDecrementCounter}
+          added={this.props.onAddCounter}
+          subtracted={this.props.onSubtractCounter}
+        />
+
+        <Divider />
+
+        <Button
+          inverted
+          color='blue'
+          onClick={() => this.props.onStoreResult(this.props.counter)}
+        >
+        Show Result
+        </Button>
+
+        <CounterShowResult resultShowed={showResult} />
+
       </div>
     )
   }
@@ -29,7 +74,8 @@ class Counter extends Component {
 const mapStateToProps = state => {
   return {
     counter: state.ctr.counter,
-    results: state.res.results
+    results: state.res.results,
+    loading: state.res.loading
   }
 }
 
