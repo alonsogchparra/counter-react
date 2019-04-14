c657361b7bcaa7d68aa88a91bd50e196a1e7e851
Alonso Parra <alonsogparra@gmail.com>
2019-04-14 17:01:22 -0400

    Updating container with redux elements

--- counter_semantic_ui/src/containers/Counter/Counter.js
+++ counter_semantic_ui/src/containers/Counter/Counter.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import * as actionCreators from '../../store/actions';
 
 class Counter extends Component {
   render () {
     return (
       <div>
-        <CounterOutput value={55} />
-        <CounterControl label="Increment" />
-        <CounterControl label="Decrement" />
-        <CounterControl label="Add" />
-        <CounterControl label="Subtract" />
+        <CounterOutput value={this.props.counter} />
+        <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+        <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
+        <CounterControl label="Add" clicked={this.props.onAddCounter} />
+        <CounterControl label="Subtract" clicked={this.props.onSubtractCounter} />
         <div>Show Result</div>
       </div>
     )
   }
 }
 
-export default Counter;
+const mapStateToProps = state => {
+  return {
+    counter: state.ctr.counter
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onIncrementCounter: () => dispatch (actionCreators.increment()),
+    onDecrementCounter: () => dispatch (actionCreators.decrement()),
+    onAddCounter: () => dispatch (actionCreators.add(10)),
+    onSubtractCounter: () => dispatch (actionCreators.subtract(15))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
