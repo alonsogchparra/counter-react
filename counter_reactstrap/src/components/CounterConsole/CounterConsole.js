3487f7b9edc1251b96459b07ad41bf5174933e0f
Alonso Parra <alonsogparra@gmail.com>
2019-04-27 12:11:25 -0400

    CounterConsole fixed for Add click function

--- counter_reactstrap/src/components/CounterConsole/CounterConsole.js
+++ counter_reactstrap/src/components/CounterConsole/CounterConsole.js
@@ -9,7 +9,7 @@ const counterConsole = (props) => (
     <Row>
       <CounterControl label='Increment' clicked={props.incremented} id='0' message='Increment 1' />
       <CounterControl label='Decrement' clicked={props.decremented} id='1' message='Decrement 1' />
-      <CounterControl label='Add' clicked={props.Added} id='2' message='Add 10' />
+      <CounterControl label='Add' clicked={props.added} id='2' message='Add 10' />
       <CounterControl label='Subtract' clicked={props.subtracted} id='3' message='Subtract 15' />
     </Row>
   </Container>
