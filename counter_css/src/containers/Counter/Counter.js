e5f6bb68d6e3ef30f050389d4f05138b7a52453c
Alonso Parra <alonsogparra@gmail.com>
2019-07-12 14:53:50 -0400

    Counter updated with redux

--- counter_css/src/containers/Counter/Counter.js
+++ counter_css/src/containers/Counter/Counter.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 import CounterControl from '../../components/CounterControl/CounterControl';
+import * as actions from '../../store/actions';
 
 class Counter extends Component {
   render () {
     return (
       <div>
-        <CounterOutput value="54545" />
-        <CounterControl label="Increment" />
-        <CounterControl label="Decrement" />
-        <CounterControl label="Add" />
-        <CounterControl label="Subtract" />
+        <CounterOutput value={this.props.counter} />
+        <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+        <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
+        <CounterControl label="Add" clicked={this.props.onAddCounter} />
+        <CounterControl label="Subtract" clicked={this.props.onSubtractCounter} />
         <button>Show Result</button>
       </div>
     )
   }
 }
 
-export default Counter
+const mapStateToProps = state => {
+  return {
+    counter: state.ctr.counter
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onIncrementCounter: () => dispatch(actions.increment()),
+    onDecrementCounter: () => dispatch(actions.decrement()),
+    onAddCounter: () => dispatch(actions.add(10)),
+    onSubtractCounter: () => dispatch(actions.subtract(15))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
