ac58b6d72ffa15ccf2900a0920bb0b0fb296ba5a
Alonso Parra <alonsogparra@gmail.com>
2019-07-14 11:25:35 -0400

    countercontrol updated with tooltip message

--- counter_css/src/components/CounterControl/CounterControl.js
+++ counter_css/src/components/CounterControl/CounterControl.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import './CounterControl.css';
+
 const counterControl = (props) => (
-  <div onClick={props.clicked}>
-    {props.label}
+  <div className="counter_control--content">
+    <button className="counter_control--btn" onClick={props.clicked}>
+      <span className="counter_control--tooltip">{props.message}</span>
+      {props.label}
+    </button>
   </div>
 );
 
