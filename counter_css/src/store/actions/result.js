3329670339cdc7d94f20c7b493aeb3052a033036
Alonso Parra <alonsogparra@gmail.com>
2019-07-14 15:21:14 -0400

    action result updated with loadResult

--- counter_css/src/store/actions/result.js
+++ counter_css/src/store/actions/result.js
@@ -3,7 +3,8 @@ import * as actionTypes from './actionTypes';
 export const saveResult = result => {
   return {
     type: actionTypes.STORE_RESULT,
-    value: result
+    value: result,
+    isLoading: false
   };
 };
 
@@ -14,10 +15,22 @@ export const deleteResult = resId => {
   };
 };
 
+export const loadResult = () => {
+  return {
+    type: actionTypes.LOAD_RESULT,
+    isLoading: true
+  }
+}
+
 export const storeResult = result => {
   return dispatch => {
+
     setTimeout(() => {
-      dispatch(saveResult(result));
-    }, 2000);
+      dispatch(loadResult());
+      setTimeout(() => {
+        dispatch(saveResult(result));
+      }, 2000);
+    }, 0);
+
   }
 }
