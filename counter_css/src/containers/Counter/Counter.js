3b5de01c3d6c7c3dd10a49d700e58adb1e153470
Alonso Parra <alonsogparra@gmail.com>
2019-07-14 11:30:53 -0400

    Container updated replacing CounterControl with CounterConsole

--- counter_css/src/containers/Counter/Counter.js
+++ counter_css/src/containers/Counter/Counter.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
-import CounterControl from '../../components/CounterControl/CounterControl';
+import CounterConsole from '../../components/CounterConsole/CounterConsole';
 import * as actions from '../../store/actions';
 
 class Counter extends Component {
@@ -11,12 +11,17 @@ class Counter extends Component {
       <div>
 
         <CounterOutput value={this.props.counter} />
-        <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-        <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
-        <CounterControl label="Add" clicked={this.props.onAddCounter} />
-        <CounterControl label="Subtract" clicked={this.props.onSubtractCounter} />
+        <CounterConsole
+          incremented={this.props.onIncrementCounter}
+          decremented={this.props.onDecrementCounter}
+          added={this.props.onAddCounter}
+          subtracted={this.props.onSubtractCounter}
+        />
 
-        <button onClick={() => this.props.onStoreResult(this.props.counter)}>Show Result</button>
+        <div>
+          <button onClick={() => this.props.onStoreResult(this.props.counter)}>Show Result</button>
+        </div>
+
 
         <ul>
           {this.props.results.map(result => <li key={result.id} onClick={() => this.props.onDeleteResult(result.id)}>{result.value}</li>)}
