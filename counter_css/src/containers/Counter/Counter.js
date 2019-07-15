8039c9deda46a58f7bca00d58ac7a0deb0ffddf2
Alonso Parra <alonsogparra@gmail.com>
2019-07-14 20:30:47 -0400

    Adding new state on container to make the theme change

--- counter_css/src/containers/Counter/Counter.js
+++ counter_css/src/containers/Counter/Counter.js
@@ -4,14 +4,27 @@ import { connect } from 'react-redux';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 import CounterConsole from '../../components/CounterConsole/CounterConsole';
 import CounterLoader from '../../components/CounterLoader/CounterLoader';
+import CounterTheme from '../../components/CounterTheme/CounterTheme';
 import * as actions from '../../store/actions';
 
 import './Counter.css';
 
 class Counter extends Component {
+
+  state = {
+    darkThemeActivate: false
+  };
+
+  changeThemeHandler = () => {
+
+    return this.setState({
+        darkThemeActivate: !this.state.darkThemeActivate
+      });
+  }
+
   render () {
 
-    let checkResult = <CounterLoader />;
+    let checkResult = <CounterLoader isDarkTheme={this.state.darkThemeActivate} />;
 
     if (!this.props.isLoading) {
       checkResult = (
@@ -32,20 +45,36 @@ class Counter extends Component {
       );
     }
 
+    if (this.state.darkThemeActivate) {
+      document.body.classList.add('dark');
+      document.body.classList.remove('default');
+    } else {
+      document.body.classList.add('default');
+      document.body.classList.remove('dark');
+    }
+
     return (
       <div>
 
-        <CounterOutput value={this.props.counter} />
+        <CounterTheme
+          changedTheme={() => this.changeThemeHandler()}
+          isDarkTheme={this.state.darkThemeActivate}
+        />
+        <CounterOutput
+          value={this.props.counter}
+          isDarkTheme={this.state.darkThemeActivate}
+        />
         <CounterConsole
           incremented={this.props.onIncrementCounter}
           decremented={this.props.onDecrementCounter}
           added={this.props.onAddCounter}
           subtracted={this.props.onSubtractCounter}
+          checkTheme={this.state.darkThemeActivate}
         />
 
         <div>
           <button
-            className="counter--btn_result"
+            className={"counter--btn_result " + (this.state.darkThemeActivate ? 'dark' : 'default')}
             onClick={() => this.props.onStoreResult(this.props.counter)}>Show Result</button>
         </div>
 
