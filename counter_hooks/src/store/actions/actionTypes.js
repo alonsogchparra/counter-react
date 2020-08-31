35ebdbcdb91f6e0d068c1d30bfb0d85cabc236a1
Alonso Parra <alonsogparra@gmail.com>
2020-08-31 16:38:17 -0400

    actionTypes fixed

--- counter_hooks/src/store/actions/actionTypes.js
+++ counter_hooks/src/store/actions/actionTypes.js
@@ -3,6 +3,6 @@ export const DECREMENT = 'DECREMENT';
 export const ADD = 'ADD';
 export const SUBTRACT = 'SUBTRACT';
 
-export const SHOW_RESULT = 'SHOW_RESULT';
+export const STORE_RESULT = 'STORE_RESULT';
 export const DELETE_RESULT = 'DELETE_RESULT';
 export const LOADING_RESULT = 'LOADING_RESULT';
