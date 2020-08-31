c33248fa6fcef139693d0ff15d5b1420ef6ec476
Alonso Parra <alonsogparra@gmail.com>
2020-08-31 16:37:48 -0400

    Adding result redux elements to Counter Component

--- counter_hooks/src/components/Counter.js
+++ counter_hooks/src/components/Counter.js
@@ -5,6 +5,8 @@ import * as actions from '../store/actions';
 const Counter = () => {
 
   const counter = useSelector((state) => state.ctr.counter);
+  const results = useSelector((state) => state.res.results);
+  const isLoading = useSelector((state) => state.res.isLoading);
   const dispatch = useDispatch();
 
   return (
@@ -14,6 +16,20 @@ const Counter = () => {
       <button onClick={() => dispatch(actions.decrement())}>Decrement</button>
       <button onClick={() => dispatch(actions.add(10))}>Add 10</button>
       <button onClick={() => dispatch(actions.subtract(15))}>Subtract 15</button>
+      <div>
+        <button onClick={() => dispatch(actions.storeResult(counter))}>Show Result</button>
+      </div>
+
+      {isLoading ? (<div>Loading...</div>) : (
+        <ul>
+          {results.map((result) => {
+            return <li key={result.id}>{result.value}
+              <button onClick={() => dispatch(actions.deleteResult(result.id))}>Delete</button>
+            </li>
+          })}
+        </ul>
+      )}
+
     </div>
   )
 }
