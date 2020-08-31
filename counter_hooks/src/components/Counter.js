d41f7d89bb0633ce5a0d24051ccac0bec54e5cee
Alonso Parra <alonsogparra@gmail.com>
2020-08-31 11:57:27 -0400

    First commit - Uploading Files

---
+++ counter_hooks/src/components/Counter.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Counter Component</h1>
+      </div>
+    );
+  }
+}
+
+export default Counter;
