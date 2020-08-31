c9640e5bd790f3452a164982487d6e6784051b50
Alonso Parra <alonsogparra@gmail.com>
2020-08-31 15:05:53 -0400

    ActionTyped added

---
+++ counter_hooks/src/store/actions/actionTypes.js
@@ -0,0 +1,8 @@
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const ADD = 'ADD';
+export const SUBTRACT = 'SUBTRACT';
+
+export const SHOW_RESULT = 'SHOW_RESULT';
+export const DELETE_RESULT = 'DELETE_RESULT';
+export const LOADING_RESULT = 'LOADING_RESULT';
