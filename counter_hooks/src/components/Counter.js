2b2493e256fff6f0c664c89fa6e210aa63ed4edc
Alonso Parra <alonsogparra@gmail.com>
2020-08-31 15:08:49 -0400

    Counter updated with hooks

--- counter_hooks/src/components/Counter.js
+++ counter_hooks/src/components/Counter.js
@@ -1,13 +1,21 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import * as actions from '../store/actions';
 
-class Counter extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Counter Component</h1>
-      </div>
-    );
-  }
+const Counter = () => {
+
+  const counter = useSelector((state) => state.ctr.counter);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>Current Value: {counter}</h1>
+      <button onClick={() => dispatch(actions.increment())}>Increment</button>
+      <button onClick={() => dispatch(actions.decrement())}>Decrement</button>
+      <button onClick={() => dispatch(actions.add(10))}>Add 10</button>
+      <button onClick={() => dispatch(actions.subtract(15))}>Subtract 15</button>
+    </div>
+  )
 }
 
 export default Counter;
